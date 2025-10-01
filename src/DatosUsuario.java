70d04d5f8eb6703166d124a72b07932c8845665b
Mokel400 <soymikelnevado@gmail.com>
2025-10-01 14:14:40 +0200

    Update DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,47 +1,51 @@
 import java.util.Scanner;
 import utilidades.ProcesadorTexto;
 import utilidades.Usuario;
-
-
 public class DatosUsuario {
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) {    
         
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.print("Introduce tu nombre: ");
         String nombre = sc.nextLine();
-
+        
         System.out.print("Introduce tu edad: ");
         int edad = sc.nextInt();
         sc.nextLine();
-
+        
         System.out.print("Introduce tu correo electrónico: ");
         String correoElectronico = sc.nextLine();
-
-
+        
         boolean emailValido = ProcesadorTexto.esEmailValido(correoElectronico);
+        boolean edadValida = edad >= 1 && edad < 120;
+        boolean nombreValido = nombre.length() > 2 && nombre.length() < 20;
+        String nombreBien = String.valueOf(nombre.charAt(0)).toUpperCase() + (nombre.substring(1).toLowerCase());
 
-        if (emailValido){
+        if(nombreValido){
+            nombreBien = nombreBien.replace(" ", "").trim();
 
-            Usuario ususario = new Usuario (nombre, edad, correoElectronico);
+            if (edadValida){
+         
+                if (emailValido){
+                Usuario ususario = new Usuario (nombreBien, edad, correoElectronico);
+                ususario.mostrarInformacion();
+        
+                } else {
+        
+                System.out.println("No se ha podido crear el ususario porque el correo introducido no es válido");
+                }
 
-            ususario.mostrarInformacion();
+            } else {
+
+                System.out.println("No se ha podido crear el usuario porque la edad introducida no es válida");
+            }
 
         } else {
 
-            System.out.println("No se ha podido crear el ususario porque el correo introducido no es válido");
-
-
+            System.out.println("No se ha podido crear el ususario porque el nombre introducido no es válida");
         }
 
-
-
         sc.close();
-
-
-
-
     }
-
 }
